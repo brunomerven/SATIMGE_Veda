a00bf7547c64af49de13bcddd6c41edf9ab51e16
Bruno <brunomerven@gmail.com>
2023-12-05 14:13:22 +0200

    dd files committed for REF, NDCUP, NDCLO

---
+++ GAMS_WrkTIMES/NZ_NDCLO/vtrun.cmd
@@ -0,0 +1,7 @@
+@echo off
+SET "$case_name=nz_ndclo"
+SET "$vd_file_name=nz_ndclo_0512"
+Title nz_ndclo [NZ_NDCLO] 
+CALL ..\..\GAMS_SrcTIMES.v4.7.0\VT_GAMS %$case_name% ..\GAMS_SrcTIMES.v4.7.0 GAMSSAVE\%$case_name% '' ..\ lo=1 2>&1 | tee "%$case_name%_run_log.txt"
+GDX2VEDA GAMSSAVE\%$case_name% ..\..\GAMS_SrcTIMES.v4.7.0\times2veda.vdd %$vd_file_name%
+@echo Closed >RunTerminated
