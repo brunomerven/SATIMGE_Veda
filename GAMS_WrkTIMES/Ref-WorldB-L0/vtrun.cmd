43cbd2355bb0402a29f99f65add5b168d30a9d45
Guy Cunliffe <guy.cunliffe@uct.ac.za>
2023-12-14 15:52:33 +0200

    Revert "Attempted a run of Ref-WorldB-L0 with CGE, got some weird AFOLU results"
    This reverts commit cd9a073f3f26e93d4ca54cf2bbec00bcc2f39f60.

--- GAMS_WrkTIMES/Ref-WorldB-L0/vtrun.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-SET "$case_name=ref-worldb-l0"
-SET "$vd_file_name=ref-worldb-l0_1412"
-Title ref-worldb-l0 [REF-WORLDB-L0] 
-CALL ..\..\GAMS_SrcTIMES.v4.7.0\VT_GAMS %$case_name% ..\GAMS_SrcTIMES.v4.7.0 GAMSSAVE\%$case_name% '' ..\ lo=1 2>&1 | tee "%$case_name%_run_log.txt"
-GDX2VEDA GAMSSAVE\%$case_name% ..\..\GAMS_SrcTIMES.v4.7.0\times2veda.vdd %$vd_file_name%
-@echo Closed >RunTerminated
