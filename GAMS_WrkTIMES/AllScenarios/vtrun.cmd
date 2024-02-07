75aac7f718addb9766ae46e9702d95660b89271b
Bruno <brunomerven@gmail.com>
2024-02-07 13:28:38 +0200

    With IRP updates (note that IPP changes from earlier commit is here)

--- GAMS_WrkTIMES/AllScenarios/vtrun.cmd
+++ GAMS_WrkTIMES/AllScenarios/vtrun.cmd
@@ -1,6 +1,6 @@
 @echo off
 SET "$case_name=allscenarios"
-SET "$vd_file_name=allscenarios_0102"
+SET "$vd_file_name=allscenarios_0602"
 Title allscenarios [ALLSCENARIOS] 
 CALL ..\..\GAMS_SrcTIMES.v4.7.0\VT_GAMS %$case_name% ..\GAMS_SrcTIMES.v4.7.0 GAMSSAVE\%$case_name% '' ..\ lo=1 2>&1 | tee "%$case_name%_run_log.txt"
 GDX2VEDA GAMSSAVE\%$case_name% ..\..\GAMS_SrcTIMES.v4.7.0\times2veda.vdd %$vd_file_name%
